c6833d730354c8512cee12f4047248c97a937a61
gitameerhamza <148001002+gitameerhamza@users.noreply.github.com>
2024-10-23 00:49:35 +0500

    additional info part done

--- lib/main.dart
+++ lib/main.dart
@@ -12,114 +12,196 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-            // scaffoldBackgroundColor: Colors.white54
-            ),
-        home: Scaffold(
-            backgroundColor: const Color.fromARGB(255, 65, 64, 64),
-            appBar: _appBar(),
-            body: Column(
-              children: [
-                Padding(
-                  padding: const EdgeInsets.all(10.0),
-                  child: SizedBox(
-                    width: 479,
-                    height: 225,
-                    child: Card(
-                        borderOnForeground: true,
-                        shape: const RoundedRectangleBorder(
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(20))),
-                        clipBehavior: Clip.hardEdge,
-                        color: Colors.blueGrey,
-                        elevation: 23,
-                        margin: const EdgeInsets.all(10),
-                        child: BackdropFilter(
-                          filter: ImageFilter.blur(sigmaX: 9, sigmaY: 9),
-                          child: const Padding(
-                            padding: EdgeInsets.all(8.0),
-                            child: Column(
-                              children: [
-                                Text(
-                                  '34.0 °C',
-                                  style: TextStyle(
-                                      fontWeight: FontWeight.bold,
-                                      fontSize: 24,
-                                      color:
-                                          Color.fromARGB(255, 255, 255, 255)),
-                                ),
-                                Icon(
-                                  Icons.cloud,
-                                  size: 99,
-                                  color: Color.fromARGB(255, 155, 155, 155),
-                                ),
-                                Text(
-                                  '9:00',
-                                  style: TextStyle(
-                                      color: Color.fromARGB(255, 255, 255, 255),
-                                      fontSize: 23),
-                                )
-                              ],
-                            ),
-                          ),
-                        )),
-                  ),
-                ),
-                Container(
-                  margin: const EdgeInsets.only(left: 20, right: 15),
-                  child: Column(
-                    children: [
-                      const Align(
-                        alignment: Alignment.centerLeft,
-                        child: Text(
-                          'Weather Forecast',
-                          style: TextStyle(
-                              fontWeight: FontWeight.bold,
-                              fontSize: 24,
-                              color: Colors.white),
-                        ),
-                      ),
-                      SingleChildScrollView(
-                        scrollDirection: Axis.horizontal,
-                        child: Row(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          // scaffoldBackgroundColor: Colors.white54
+          ),
+      home: Scaffold(
+        backgroundColor: const Color.fromARGB(255, 65, 64, 64),
+        appBar: _appBar(),
+        body: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: SizedBox(
+                width: 479,
+                height: 225,
+                child: Card(
+                    borderOnForeground: true,
+                    shape: const RoundedRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(20))),
+                    clipBehavior: Clip.hardEdge,
+                    color: Colors.blueGrey,
+                    elevation: 23,
+                    margin: const EdgeInsets.all(10),
+                    child: BackdropFilter(
+                      filter: ImageFilter.blur(sigmaX: 9, sigmaY: 9),
+                      child: const Padding(
+                        padding: EdgeInsets.all(8.0),
+                        child: Column(
                           children: [
-                            newwetherCard(),
-                            const SizedBox(width: 10),
-                            newwetherCard(),
-                            const SizedBox(width: 10),
-                            newwetherCard(),
-                            const SizedBox(width: 10),
-                            newwetherCard(),
-                            const SizedBox(width: 10),
-                            newwetherCard(),
-                            const SizedBox(width: 10),
-                            newwetherCard()
+                            Text(
+                              '34.0 °C',
+                              style: TextStyle(
+                                  fontWeight: FontWeight.bold,
+                                  fontSize: 24,
+                                  color: Color.fromARGB(255, 255, 255, 255)),
+                            ),
+                            Icon(
+                              Icons.cloud,
+                              size: 99,
+                              color: Color.fromARGB(255, 155, 155, 155),
+                            ),
+                            Text(
+                              '9:00',
+                              style: TextStyle(
+                                  color: Color.fromARGB(255, 255, 255, 255),
+                                  fontSize: 23),
+                            )
                           ],
                         ),
-                      )
+                      ),
+                    )),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 20, right: 15),
+              child: Column(
+                children: [
+                  const Align(
+                    alignment: Alignment.centerLeft,
+                    child: Text(
+                      'Weather Forecast',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          color: Colors.white),
+                    ),
+                  ),
+                  SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    child: Row(
+                      children: [
+                        newwetherCard(),
+                        const SizedBox(width: 10),
+                        newwetherCard(),
+                        const SizedBox(width: 10),
+                        newwetherCard(),
+                        const SizedBox(width: 10),
+                        newwetherCard(),
+                        const SizedBox(width: 10),
+                        newwetherCard(),
+                        const SizedBox(width: 10),
+                        newwetherCard()
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.only(left: 22, top: 10),
+              alignment: Alignment.centerLeft,
+              child: const Text(
+                'Additional Information',
+                style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 23,
+                    color: Colors.white),
+              ),
+            ),
+             const Row(
+              children: [
+                Padding(
+                  padding: EdgeInsets.only(left: 65, top: 20),
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.water_drop_rounded,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Humidty',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Text('94',
+                      style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,
+                      color: Colors.white) ,)
                     ],
                   ),
                 ),
-                // Placeholder(),
-                // Placeholder(),
+                Padding(
+                  padding: EdgeInsets.only(left: 55, top: 20),
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.air_outlined,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Wind Speed',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Text('7.7',
+                      style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,
+                      color: Colors.white) ,)
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 55, top: 20),
+                  child: Column(
+                    children: [
+                      Icon(
+                        Icons.umbrella_sharp,
+                        size: 40,
+                        color: Colors.white,
+                      ),
+                      SizedBox(
+                        height: 5,
+                      ),
+                      Text(
+                        'Wind Speed',
+                        style: TextStyle(color: Colors.grey),
+                      ),
+                      Text('1006',
+                      style:TextStyle(fontSize: 20,fontWeight: FontWeight.bold,
+                      color: Colors.white) ,)
+                    ],
+                  ),
+                ),
               ],
-            )));
+            )
+          ],
+        ),
+      ),
+    );
   }
 
   AppBar _appBar() {
     return AppBar(
-            centerTitle: true,
-            backgroundColor: const Color.fromARGB(255, 65, 64, 64),
-            title: const Text(
-              'Weather App',
-              style:
-                  TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
-            ),
-            actions: [
-                IconButton(onPressed: (){}, icon: const Icon(Icons.refresh),color: Colors.white,)
-            ],
-          );
+      centerTitle: true,
+      backgroundColor: const Color.fromARGB(255, 65, 64, 64),
+      title: const Text(
+        'Weather App',
+        style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.refresh),
+          color: Colors.white,
+        )
+      ],
+    );
   }
 
   SizedBox newwetherCard() {
@@ -129,7 +211,7 @@ class MyApp extends StatelessWidget {
       child: Card(
         color: Colors.blueGrey,
         shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(23)),
-        child:  const Padding(
+        child: const Padding(
           padding: EdgeInsets.all(8.0),
           child: Column(
             children: [
